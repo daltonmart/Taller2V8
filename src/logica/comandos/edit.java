11f08babb3d1436bee529ec1d6ac29b42fe299f2
sacunaf <you@example.com>
2019-11-13 12:42:02 -0300

    Merge origin/VersionConTerminal
    Conflicts:
    	lib/absolutelayout/AbsoluteLayout.jar
    	lib/nblibraries.properties
    	src/src/presentacion/BorrarUsuario.form
    	src/src/presentacion/BorrarUsuario.java
    	src/src/presentacion/ControladorSesionVentana.java

--- src/logica/comandos/edit.java
+++ src/logica/comandos/edit.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package logica.comandos;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import javax.swing.JFrame;
 import logica.Comando;
 import logica.DataArchivo;
 import logica.EstructuraArchivos;
@@ -15,7 +9,6 @@ import static logica.ISistemaPrincipalImpl.red;
 import presentacion.EditorTexto;
 
 public class edit extends Comando {
-
     private String[] args;
 
     public edit() {
@@ -27,18 +20,8 @@ public class edit extends Comando {
 
     @Override
     public String ejecutarComando() {
-       /* JFrame frame = new EditorTexto_viejo();
-        
-        frame.setTitle("Editor");
-        frame.setVisible(true);
-        frame.setSize(500, 320);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(true);
-        frame.setLocationRelativeTo(null);
-*/
         StringWriter salida = new StringWriter();
         PrintWriter pw = new PrintWriter(salida);
-
         EstructuraArchivos estructArchivos = red.getEquipoActual().getCompuestoPorUsuarios().buscarUsuarioConectado().getCompuestoPorArchivos();
 
         String urlEntrada = extraerUrlDeArgs(args);
@@ -49,10 +32,7 @@ public class edit extends Comando {
             DataArchivo archivo = estructArchivos.getArchivoDeUrl(url);
             if (archivo != null) {
                 if (archivo.getTipo() == 1) {
-                    //EditorTexto editor = new EditorTexto_viejo(archivo);
-                    EditorTexto editor = new EditorTexto(archivo);
-                 //   estructArchivos.setDirectorioDeTrabajo(directorio);
-                    
+                    EditorTexto editor = new EditorTexto(archivo);                   
                 } else {
                     pw.println("No es un archivo");
                 }
